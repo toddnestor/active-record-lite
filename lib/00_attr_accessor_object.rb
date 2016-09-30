ddc87173b7754619d45de34cb11da6aea01ee054
Todd Nestor <todd.nestor@gmail.com>
2016-09-30 11:23:39 -0700

    attr accessor written

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,22 @@
 class AttrAccessorObject
+  def self.my_attr_reader(*names)
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+    end
+  end
+
+  def self.my_attr_writer(*names)
+    names.each do |name|
+      define_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
+      end
+    end
+  end
+
   def self.my_attr_accessor(*names)
-    # ...
+    self.my_attr_reader(*names)
+    self.my_attr_writer(*names)
   end
 end
