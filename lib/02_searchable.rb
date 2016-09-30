82525ceea020507269724e5fa5e7cb35ae329a8a
App Academy Student <student@appacademy.io>
2016-09-30 15:58:31 -0700

    we have a relation class now

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -1,19 +1,11 @@
 require_relative 'db_connection'
 require_relative '01_sql_object'
+require_relative 'relation'
 
 module Searchable
   def where(params)
-    sql = build_select + build_from
-
-    wheres = []
-
-    params.each do |key, val|
-      wheres << {col: key, value: '?'}
-    end
-
-    sql += self.build_where(wheres)
-
-    get_objects(sql, params.values)
+    relation = Relation.new(self)
+    relation.where(params)
   end
 end
 
